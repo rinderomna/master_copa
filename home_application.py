698fc293e95b29c3dc1e3d0cda373464f22365e9
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2022-07-13 20:33:08 -0300

    Album Teste Paginado

--- home_application.py
+++ home_application.py
@@ -1,6 +1,8 @@
 import PySimpleGUI as sg
 from PIL import Image
 
+from album import Album
+
 def resize_image(filename, basewidth, height=None):
     img = Image.open(filename)
     
@@ -21,37 +23,64 @@ layout = [
 
 window = sg.Window("Master Copa", layout, element_justification='c')
 
-def show_album_window():
+def show_album_window(album: Album):
     resize_image("images/placeholder.png", 200, 350)
+
+    total_pages = 10
+    page_index = 0 # page_index = page_number - 1 [from 0 to (total_pages - 1)]
+
     album_layout = [
-        [sg.Text("Página 1")],
+        [sg.Text("Página 1", key="-SHOW PAGE-")],
         [
-            sg.Image(source="images/placeholder.png", key="-IMAGE1-"),
-            sg.Image(source="images/placeholder.png", key="-IMAGE2-"),
-            sg.Image(source="images/placeholder.png", key="-IMAGE3-") 
+            sg.Image(source="images/1/1.png", key="-IMAGE1-"),
+            sg.Image(source="images/1/2.png", key="-IMAGE2-"),
+            sg.Image(source="images/1/3.png", key="-IMAGE3-") 
         ],
 
         [
-            sg.Image(source="images/placeholder.png", key="-IMAGE4-"),
-            sg.Image(source="images/placeholder.png", key="-IMAGE5-"),
-            sg.Image(source="images/placeholder.png", key="-IMAGE6-") 
-        ]
+            sg.Image(source="images/1/4.png", key="-IMAGE4-"),
+            sg.Image(source="images/1/5.png", key="-IMAGE5-"),
+            sg.Image(source="images/1/6.png", key="-IMAGE6-") 
+        ],
+        [sg.Button("<<<"), sg.Button(">>>")]
     ]
 
     album_window = sg.Window("Album de Figurinhas", album_layout, element_justification='c')
 
     while True: 
         event, values = album_window.read()
+
         if event == sg.WIN_CLOSED:
-          break
+            break
+        elif event == "<<<":
+            page_index = (total_pages + page_index - 1) % total_pages
+        elif event == ">>>":
+            page_index = (page_index + 1) % total_pages
+        
+        album_window["-SHOW PAGE-"].update(f"Página {page_index + 1}")
+        
+        resize_image(f"images/{album.folders[page_index]}/1.png", 200, 350)
+        resize_image(f"images/{album.folders[page_index]}/2.png", 200, 350)
+        resize_image(f"images/{album.folders[page_index]}/3.png", 200, 350)
+        resize_image(f"images/{album.folders[page_index]}/4.png", 200, 350)
+        resize_image(f"images/{album.folders[page_index]}/5.png", 200, 350)
+        resize_image(f"images/{album.folders[page_index]}/6.png", 200, 350)
+        album_window["-IMAGE1-"].update(source=f"images/{album.folders[page_index]}/1.png")
+        album_window["-IMAGE2-"].update(source=f"images/{album.folders[page_index]}/2.png")
+        album_window["-IMAGE3-"].update(source=f"images/{album.folders[page_index]}/3.png")
+        album_window["-IMAGE4-"].update(source=f"images/{album.folders[page_index]}/4.png")
+        album_window["-IMAGE5-"].update(source=f"images/{album.folders[page_index]}/5.png")
+        album_window["-IMAGE6-"].update(source=f"images/{album.folders[page_index]}/6.png")
 
     album_window.close()
+
+album = Album() # Create an album
 
 while True: 
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Sair":
         break
     elif event == "Album":
-        show_album_window()
+        show_album_window(album)
 
 window.close()
