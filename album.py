eba8fdf46bbc20ab619a7c5c137c20b23e078ad3
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2022-07-14 17:25:50 -0300

    acompanhando nomes csv

--- album.py
+++ album.py
@@ -87,42 +87,40 @@ def show_album_window(album: Album):
         [sg.Image(source=source1, key="-IMAGE1-")],
         [sg.Text(f"{album.names[0][0]}", key="-PLAYER1-")],
         [sg.Image(source=source5, key="-IMAGE5-")],
-        [sg.Text(f"{album.names[0][4]}")],
+        [sg.Text(f"{album.names[0][4]}", key="-PLAYER5-")],
         [sg.Image(source=source9, key="-IMAGE9-")],
-        [sg.Text(f"{album.names[0][8]}")]
+        [sg.Text(f"{album.names[0][8]}", key="-PLAYER9-")]
     ]
 
     column2_layout = [
         [sg.Image(source=source2, key="-IMAGE2-")],
-        [sg.Text(f"{album.names[0][1]}")],
+        [sg.Text(f"{album.names[0][1]}", key="-PLAYER2-")],
         [sg.Image(source=source6, key="-IMAGE6-")],
-        [sg.Text(f"{album.names[0][5]}")],
+        [sg.Text(f"{album.names[0][5]}", key="-PLAYER6-")],
         [sg.Image(source=source10, key="-IMAGE10-")],
-        [sg.Text(f"{album.names[0][9]}")]
+        [sg.Text(f"{album.names[0][9]}", key="-PLAYER10-")]
     ]
 
     column3_layout = [
         [sg.Image(source=source3, key="-IMAGE3-")],
-        [sg.Text(f"{album.names[0][2]}")],
+        [sg.Text(f"{album.names[0][2]}", key="-PLAYER3-")],
         [sg.Image(source=source7, key="-IMAGE7-")],
-        [sg.Text(f"{album.names[0][6]}")],
+        [sg.Text(f"{album.names[0][6]}", key="-PLAYER7-")],
         [sg.Image(source=source11, key="-IMAGE11-")],
-        [sg.Text(f"{album.names[0][10]}")]
+        [sg.Text(f"{album.names[0][10]}", key="-PLAYER11-")]
     ]
 
     column4_layout = [
         [sg.Image(source=source4, key="-IMAGE4-")],
-        [sg.Text(f"{album.names[0][3]}")],
+        [sg.Text(f"{album.names[0][3]}", key="-PLAYER4-")],
         [sg.Image(source=source8, key="-IMAGE8-")],
-        [sg.Text(f"{album.names[0][7]}")],
+        [sg.Text(f"{album.names[0][7]}", key="-PLAYER8-")],
         [sg.Image(source=source12, key="-IMAGE12-")],
-        [sg.Text(f"{album.names[0][11]}")]
+        [sg.Text(f"{album.names[0][11]}", key="-PLAYER12-")]
     ]
     
-
-
     album_layout = [
-        [sg.Text("Página 1", key="-SHOW PAGE-")],
+        [sg.Text("Página 1", key="-SHOW PAGE-"), sg.Text(f"- Time: {album.folders[0]}", key="-TIME-")],
 
         [
             sg.Column(column1_layout, element_justification="c"),
@@ -147,6 +145,7 @@ def show_album_window(album: Album):
             page_index = (page_index + 1) % total_pages
         
         album_window["-SHOW PAGE-"].update(f"Página {page_index + 1}")
+        album_window["-TIME-"].update(f"- Time: {album.folders[page_index]}")
 
         resize_image(f"images/{album.folders[page_index]}/1.png", pr.width, pr.height)
         resize_image(f"images/{album.folders[page_index]}/2.png", pr.width, pr.height)
@@ -187,6 +186,19 @@ def show_album_window(album: Album):
         album_window["-IMAGE11-"].update(source=source11)
         album_window["-IMAGE12-"].update(source=source12)
 
+        album_window["-PLAYER1-"].update(album.names[page_index][0])
+        album_window["-PLAYER2-"].update(album.names[page_index][1])
+        album_window["-PLAYER3-"].update(album.names[page_index][2])
+        album_window["-PLAYER4-"].update(album.names[page_index][3])
+        album_window["-PLAYER5-"].update(album.names[page_index][4])
+        album_window["-PLAYER6-"].update(album.names[page_index][5])
+        album_window["-PLAYER7-"].update(album.names[page_index][6])
+        album_window["-PLAYER8-"].update(album.names[page_index][7])
+        album_window["-PLAYER9-"].update(album.names[page_index][8])
+        album_window["-PLAYER10-"].update(album.names[page_index][9])
+        album_window["-PLAYER11-"].update(album.names[page_index][10])
+        album_window["-PLAYER12-"].update(album.names[page_index][11])
+
     album_window.close()
 
 def show_replicated_window(album: Album, original_window: sg.Window):
@@ -194,11 +206,23 @@ def show_replicated_window(album: Album, original_window: sg.Window):
     for replicated in album.replicated:
         values.append(f"Figurinha {replicated.page_index + 1}-{replicated.fig_index + 1} - qtd: {replicated.amount}")
     
-    replicated_layout = [
+    col1_rep = [
         [sg.Listbox(
             values=values, enable_events=True, size=(40,20),
             key="-REPLICATED LIST-"
         )]
+    ]
+
+    col2_rep = [
+        [sg.Button("Trocar Selecionado")]
+    ]
+
+    replicated_layout = [
+        [
+            sg.Column(col1_rep),
+            sg.VSeparator(),
+            sg.Column(col2_rep, vertical_alignment='c', element_justification='c')
+        ]
     ]
     
     replicated_window = sg.Window("Figurinhas Repetidas", replicated_layout, font = pr.font)
@@ -208,6 +232,11 @@ def show_replicated_window(album: Album, original_window: sg.Window):
 
         if event == sg.WIN_CLOSED:
             break
+        elif event == "Trocar Selecionado":
+            selected_index = replicated_window.Element("-REPLICATED LIST-").Widget.curselection()
+
+            print(selected_index)
+            
 
     replicated_window.close()
 
